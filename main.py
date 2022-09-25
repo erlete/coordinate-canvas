0574bb37521ff77bccd62ea1de4b473c7ec29e6e
erlete <dev.szblzpaulo@gmail.com>
2022-09-25 21:25:20 +0200

    Add documentation and commments
    Added some comments and updated class documentation. Also renamed some attributes.

--- main.py
+++ main.py
@@ -12,7 +12,6 @@ import json
 from matplotlib import pyplot as plt
 
 
-COLORS = [
     "darkorange",
     "darkblue",
     "darkgreen",
@@ -23,32 +22,39 @@ COLORS = [
 
 
 class LineBuilder:
+    """Builds a line based on click event handlers.
+
+    This class is used to build a line based on click events. The line is
+    built by clicking on the matplotlib plot. The coordinates are stored
+    in a list of tuples.
+    """
+
     def __init__(self, line):
         self.line = line
-        self.xs = list(line.get_xdata())
-        self.ys = list(line.get_ydata())
+        self.x, self.y = list(line.get_xdata()), list(line.get_ydata())
+
         self.cid = line.figure.canvas.mpl_connect("button_press_event", self)
         self.line.figure.canvas.draw()
 
     def __call__(self, event):
-        print(f"[Event] Type: click - Descriptor: {event}")
-
         if event.inaxes != self.line.axes:
             return
 
-        self.xs.append(event.xdata)
-        self.ys.append(event.ydata)
+        self.x.append(event.xdata)
+        self.y.append(event.ydata)
 
-        self.line.set_data(self.xs, self.ys)
+        self.line.set_data(self.x, self.y)
         self.line.figure.canvas.draw()
 
 
 if __name__ == "__main__":
-    # Input:
+
+    # Parameter input:
     width = float(input("Enter width: "))
     height = float(input("Enter height: "))
     line_no = int(input("Enter the number of lines to draw: "))
 
+    # Data output template:
     data = {
         f"line_{index + 1}": {
             "x": [],
@@ -57,12 +63,15 @@ if __name__ == "__main__":
     }
 
     for index in range(line_no):
+
+        # Figure setup:
         fig, ax = plt.subplots()
         plt.grid(True)
         ax.set_xlim(0, width)
         ax.set_ylim(0, height)
         ax.set_title(f"Click to add points for line number {index + 1}...")
 
+        # Previous drawings' plotting:
         if index > 0:
             for sub_index in range(index):
                 plt.plot(
@@ -71,23 +80,23 @@ if __name__ == "__main__":
                     "--",
                     marker="2",
                     markersize=5,
-                    color=COLORS[sub_index]
                 )
 
+        # Line drawing and display:
         line, = ax.plot(
             [], [],
             "--",
             marker="2",
             markersize=5,
-            color=COLORS[index]
+            color=LINE_COLORS[index]
         )
-
         builder = LineBuilder(line)
-
         plt.show()
 
-        data[f"line_{index + 1}"]['x'].extend(builder.xs)
-        data[f"line_{index + 1}"]['y'].extend(builder.ys)
+        # Data storage:
+        data[f"line_{index + 1}"]['x'].extend(builder.x)
+        data[f"line_{index + 1}"]['y'].extend(builder.y)
 
+    # Data output:
     with open("coordinates.json", "w", encoding="utf-8") as output:
         json.dump(data, output, ensure_ascii=False, indent=4)
