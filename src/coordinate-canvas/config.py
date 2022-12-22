f1b79a84611e107f3b31c8e3c94d6f7d843e7371
Paulo Sánchez <dev.szblzpaulo@gmail.com>
2022-12-22 12:25:12 +0100

    Add configuration module

---
+++ src/coordinate-canvas/config.py
@@ -0,0 +1,16 @@
+CONFIG = {
+    "colors": [
+        "red", "green", "salmon", "blue",
+        "orange", "violet", "goldenrod", "gray"
+    ],
+    "input": {
+        "shape": "o",
+        "size": 5,
+        "alpha": 1
+    },
+    "positions": {
+        "shape": "-",
+        "size": 1,
+        "alpha": 0.5
+    }
+}
