b0157845ff639cbc073539c3f7e81fdb08acacd8
Paulo Sánchez <dev.szblzpaulo@gmail.com>
2023-03-05 05:17:02 +0100

    Update documentation requirements
    Added `sphinx-rtd-theme` to the documentation configuration.

--- docs/conf.py
+++ docs/conf.py
@@ -8,6 +8,8 @@ import os
 import pathlib
 import sys
 
+import coordinate_canvas
+
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
@@ -16,11 +18,9 @@ COORDINATE_CANVAS_PATH = CURRENT_PATH.parent.parent
 
 sys.path.insert(0, str(COORDINATE_CANVAS_PATH))
 
-import coordinate_canvas
-
-project = 'coordinate-canvas'
+project = "coordinate-canvas"
 copyright = f"{datetime.date.today().year}, Paulo Sanchez and Contrbutors"
-author = 'Paulo Sanchez'
+author = "Paulo Sanchez"
 release = coordinate_canvas.__version__
 
 # -- General configuration ---------------------------------------------------
@@ -35,30 +35,29 @@ extensions = [
     "sphinx.ext.viewcode",
     "sphinx.ext.autosummary",
     # "sphinx_tabs.tabs",
-    'sphinx.ext.ifconfig',
+    "sphinx.ext.ifconfig",
+    "sphinx_rtd_theme"
 ]
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
-
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+html_theme = "sphinx_rtd_theme"
+html_static_path = ["_static"]
 
 html_theme_options = {
-    'github_user': 'erlete',
-    'github_repo': 'coordinate-canvas',
-    'github_banner': True,
-    'github_button': True,
-    'github_type': 'star',
-    'fixed_sidebar': True,
+    "github_user": "erlete",
+    "github_repo": "coordinate-canvas",
+    "github_banner": True,
+    "github_button": True,
+    "github_type": "star",
+    "fixed_sidebar": True,
 }
 
 autodoc_default_options = {
-    'member-order': 'bysource',
+    "member-order": "bysource",
 }
 
 add_module_names = False
