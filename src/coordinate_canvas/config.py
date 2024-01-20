2284279a0b7cf5bf799bb25248f177fb43b54a9a
Paulo Sanchez <dev.szblzpaulo@gmail.com>
2024-01-20 02:30:12 +0100

    Update config

---
+++ src/coordinate_canvas/config.py
@@ -0,0 +1,18 @@
+class Input:
+
+    SHAPE = "0"
+    SIZE = 5
+    ALPHA = 1
+
+
+class Positions:
+
+    SHAPE = "-"
+    SIZE = 1
+    ALPHA = 0.5
+
+
+COLORS = [
+    "red", "green", "salmon", "blue",
+    "orange", "violet", "goldenrod", "gray"
+]
